e4a11aba77ecb255190270905ca41f654e773d83
Vas Jaremchuk <vas@speedandfunction.com>
2022-06-26 08:21:37 +0300

    Pantheon endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@
 export default function config() {
   const serverConfig = {
     // Base URL of your Drupal site. No tailing slash!!!
-    base: 'https://event-server.lndo.site',
+    base: 'https://dev-event-app.pantheonsite.io',
     // Name to use when storing the token in localStorage.
     token_name: 'drupal-oauth-token',
     // OAuth client ID - get from Drupal.
